5f3a8828ffeafba204db07bbf4eda3fc0a506efb
Ekwoge Blaise <ekwogeblaise.1@gmail.com>
2020-04-29 13:32:08 +0100

    Pagination added to fetch first 3 records only

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+# from tasks.views import IndexView
 
 urlpatterns = [
+    # path('', IndexView.as_view(), name='home'),
     path('', views.index, name='home'),
     path('update_task/<str:pk>/', views.updateTask, name='update'),
     path('delete/<str:pk>/', views.delete, name='delete'),
--- tasks/views.py
+++ tasks/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Task
 from .forms import TaskForm
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
-# Create your views here.
+# from django.views import generic
 def index(request):
     tasks = Task.objects.order_by('-id').all()
     page = request.GET.get('page', 1)
@@ -19,7 +18,7 @@ def index(request):
         return redirect('/')
 
     # Testing pagination
-    paginator = Paginator(tasks, 4)
+    paginator = Paginator(tasks, 3)
     try:
         tasks = paginator.page(page)
     except PageNotAnInteger:
@@ -30,6 +29,12 @@ def index(request):
     # Create a dictionary to hold all values
     context = {'tasks': tasks, 'form':form}
     return render(request, 'tasks/index.html', context)
+# class IndexView(generic.ListView):
+#     model = Task
+#     template_name = 'tasks/index.html'  # Default: <app_label>/<model_name>_list.html
+#     context_object_name = 'tasks'  # Default: object_list
+#     paginate_by = 4
+#     queryset = Task.objects.all()
 
 def updateTask(request, pk):
     task = Task.objects.get(id=pk)

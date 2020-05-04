da62608fac940b7f3a93e3c2a9f24d39285c5a2f
Ekwoge Blaise <ekwogeblaise.1@gmail.com>
2020-05-04 10:06:55 +0100

    Changed debug mode back to True

--- to_do_list/settings.py
+++ to_do_list/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'py5$&=%t63&s!j*jb^)=kv-6_a^iq%mf72hhnf*o#q8v$w*90v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
